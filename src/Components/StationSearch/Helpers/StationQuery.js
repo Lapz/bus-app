244f5903010bffb4ba87e1da08b7a415f4c08759
Lenard Pratt <striderman34@gmail.com>
2017-01-06 20:40:33 +0000

    Edir

--- src/Components/StationSearch/Helpers/StationQuery.js
+++ src/Components/StationSearch/Helpers/StationQuery.js
@@ -2,6 +2,6 @@ function createStationQueryUrl(query) {
 	return `https://api.tfl.gov.uk/Stoppoint/search/${query}?modes=tube`;
 }
 function createOutBoundUrl(id) {
-	return `https://api.tfl.gov.uk/StopPoint/${id}/Arrivals?mode=tube`
+	return `https://api.tfl.gov.uk/StopPoint/${id}/`
 }
 export {createStationQueryUrl, createOutBoundUrl}
