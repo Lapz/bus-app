b7be963fee86c8d81a90a5825208465c39c045d6
Lapz <striderman34@gmail.com>
2017-10-13 17:45:58 +0100

    Added mising functio

--- src/Components/LineItem.jsx
+++ src/Components/LineItem.jsx
@@ -1,6 +1,46 @@
 import React, {Component} from 'react';
-import {getColourCode} from "./Helpers/refreshTime"
 
+function getColourCode(stationName){
+    switch(stationName){
+        case "Bakerloo":
+            return "#894E24";
+          
+        case "Central":
+            return "#DC241F";
+           
+        case "Circle":
+            return "#FFCE00"
+       
+        case  "District":
+            return "#007229"
+           
+        case "Hammersmith & City":
+            return "#D087A1"
+           
+        case "Jubilee":
+            return "#6A7278"
+           
+        case "Metropolitan":
+            return "#751056"
+           
+        case "Northern":
+            return "#000000"
+           
+        case "Piccadilly":
+            return "#00158F"
+           
+        case "Victoria":
+            return "#00A0E2"
+            
+        case "Waterloo & City":
+            return "#76D0BD"
+            
+        default:
+            return ""
+           
+    }
+    
+}
 
 const LineItem = ({props}) => (
   <li style={getColourCode(props.lineName)}>
